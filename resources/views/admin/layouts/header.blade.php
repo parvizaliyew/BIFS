3f4c1f31c2de36c58426b156059487f294acafe9
parvizaliyew <eliyevperviz466@gmail.com>
2022-11-05 10:43:17 +0400

    Admin paneli parcaladim

---
+++ resources/views/admin/layouts/header.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<!-- Mirrored from demo.dashboardpack.com/marketing-html/index_3.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 02 Aug 2022 06:20:48 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<title>@yield('title')</title>
+<link rel="icon" href="{{asset('manager/')}}/img/logo.png" type="image/png">
+
+<link rel="stylesheet" href="{{asset('manager/')}}/css/bootstrap1.min.css" />
+
+<link rel="stylesheet" href="{{asset('manager/')}}/vendors/themefy_icon/themify-icons.css" />
+
+<link rel="stylesheet" href="{{asset('manager/')}}/vendors/niceselect/css/nice-select.css" />
+
+<link rel="stylesheet" href="{{asset('manager/')}}/vendors/owl_carousel/css/owl.carousel.css" />
+
+<link rel="stylesheet" href="{{asset('manager/')}}/vendors/gijgo/gijgo.min.css" />
+
+<link rel="stylesheet" href="{{asset('manager/')}}/vendors/font_awesome/css/all.min.css" />
+<link rel="stylesheet" href="{{asset('manager/')}}/vendors/tagsinput/tagsinput.css" />
+
+<link rel="stylesheet" href="{{asset('manager/')}}/vendors/datepicker/date-picker.css" />
+
+<link rel="stylesheet" href="{{asset('manager/')}}/vendors/scroll/scrollable.css" />
+
+<link rel="stylesheet" href="{{asset('manager/')}}/vendors/datatable/css/jquery.dataTables.min.css" />
+<link rel="stylesheet" href="{{asset('manager/')}}/vendors/datatable/css/responsive.dataTables.min.css" />
+<link rel="stylesheet" href="{{asset('manager/')}}/vendors/datatable/css/buttons.dataTables.min.css" />
+
+<link rel="stylesheet" href="{{asset('manager/')}}/vendors/text_editor/summernote-bs4.css" />
+
+<link rel="stylesheet" href="{{asset('manager/')}}/vendors/morris/morris.css">
+
+<link rel="stylesheet" href="{{asset('manager/')}}/vendors/material_icon/material-icons.css" />
+
+<link rel="stylesheet" href="{{asset('manager/')}}/css/metisMenu.css">
+
+<link rel="stylesheet" href="{{asset('manager/')}}/css/style1.css" />
+<link rel="stylesheet" href="{{asset('manager/')}}/css/colors/default.css" id="colorSkinCSS">
+</head>
+<body class="crm_body_bg">
+<section class="main_content dashboard_part large_header_bg">
---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,20 @@
+<nav class="sidebar dark_sidebar vertical-scroll  ps-container ps-theme-default ps-active-y">
+    <div class="logo d-flex justify-content-between">
+    <a href="index.html"><img src="{{asset('manager/')}}/img/logo_white.png" alt=""></a>
+    <div class="sidebar_close_icon d-lg-none">
+    <i class="ti-close"></i>
+    </div>
+    </div>
+    <ul id="sidebar_menu">
+    <li class="mm-active">
+    <a  href="{{ route('admin.index') }}" aria-expanded="false">
+    <div class="icon_menu">
+    <img src="{{asset('manager/')}}/img/menu-icon/dashboard.svg" alt="">
+    </div>
+    <span>Admin</span>
+    </a>
+    </li> 
+    
+    </ul>
+    </nav>
+    
